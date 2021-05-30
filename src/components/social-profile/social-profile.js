15debfd2fadcb55795bb5d00afc2f3449e109c2e
Volodymyr Viter <mad.tvido@gmail.com>
2021-05-30 23:53:07 +0300

    upd

--- src/components/social-profile/social-profile.js
+++ src/components/social-profile/social-profile.js
@@ -3,55 +3,55 @@ import PropTypes from 'prop-types';
 import defaultAva from '../../img/defaultAva.jpg';
 
 const Profile = ({ avatar, name, tag, location, followers, views, likes }) => {
-    return (
-        <div className="profile">
-            <div className="description">
-                <img
-                    src={avatar}
-                    alt="Аватар пользователя"
-                    className="avatar"
-                />
-                <p className="name">{name}</p>
-                <p className="tag">@{tag}</p>
-                <p className="location">{location}</p>
-            </div>
+  return (
+    <div className="profile">
+      <div className="description">
+        <img
+          src={avatar}
+          alt="Аватар пользователя"
+          className="avatar"
+        />
+        <p className="name">{name}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
 
-            <ul className="stats">
-                <li>
-                    <span className="label">Followers</span>
-                    <span className="quantity">{followers}</span>
-                </li>
-                <li>
-                    <span className="label">Views</span>
-                    <span className="quantity">{views}</span>
-                </li>
-                <li>
-                    <span className="label">Likes</span>
-                    <span className="quantity">{likes}</span>
-                </li>
-            </ul>
-        </div>
-    )
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 Profile.defaultProps = {
-    avatar: defaultAva,
-    name: "NoName",
-    tag: "tag",
-    location: "Nowhere",
-    followers: "Zero",
-    views: "Zero",
-    likes: "Zero",
+  avatar: defaultAva,
+  name: "NoName",
+  tag: "tag",
+  location: "Nowhere",
+  followers: "Zero",
+  views: "Zero",
+  likes: "Zero",
 };
 
 Profile.propTypes = {
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    followers: PropTypes.number.isRequired,
-    views: PropTypes.number.isRequired,
-    likes: PropTypes.number.isRequired,
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
 };
 
 export default Profile;
