02e75bba9f35e6751700bca66ec6dcb007b5cdea
Volodymyr Viter <mad.tvido@gmail.com>
2021-05-30 22:30:28 +0300

    upd HW1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Social from './components/social-profile/social-profile'
+
+
+const App = () => {
+  return (
+    <div>
+      <Social />
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+// import PropTypes from 'prop-types';
 
-const socialJSX = (
-  <div class="profile">
-    <div class="description">
-      <img
-        src="https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg"
-        alt="Аватар пользователя"
-        class="avatar"
-      />
-      <p class="name">Petra Marica</p>
-      <p class="tag">@pmarica</p>
-      <p class="location">Salvador, Brasil</p>
-    </div>
+// X.propTypes = {
+//     X: PropTypes.string.isRequired
+// }
 
-    <ul class="stats">
-      <li>
-        <span class="label">Followers</span>
-        <span class="quantity">1000</span>
-      </li>
-      <li>
-        <span class="label">Views</span>
-        <span class="quantity">2000</span>
-      </li>
-      <li>
-        <span class="label">Likes</span>
-        <span class="quantity">3000</span>
-      </li>
-    </ul>
-  </div>
-);
+// X.defaultProps = {
+//     X: 'None'
+// }
 
-ReactDOM.render(socialJSX, document.querySelector('#root'));
+// ReactDOM.render(<App/>, document.getElementById("root"));
+
+ReactDOM.render(<App/>, document.getElementById("root"));
