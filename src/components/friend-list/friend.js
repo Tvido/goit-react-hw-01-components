2651586def2d66897df4389ebbdb9ed69c36522c
Volodymyr Viter <mad.tvido@gmail.com>
2021-06-02 22:36:40 +0300

    upd

--- src/components/friend-list/friend.js
+++ src/components/friend-list/friend.js
@@ -6,7 +6,9 @@ import "./friend-list.css";
 
 const FriendListItem = ({ isOnline, avatar, name }) => (
   <li className="friend__list-item">
-    <span className={isOnline ? "friend-list__status friend__list-status-online" : "friend-list__status friend__list-status-offline"} />
+    <span className="friend__list-status" style={{ backgroundColor: isOnline === true ? "green" : "red"}} />
+
+
     <img className="avatar" src={avatar} alt="" width="48" />
     <p className="friend__list-name">{name}</p>
   </li>
